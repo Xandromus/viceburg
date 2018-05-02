b0a564c0967c869cd6838cea99f5049edb662c59
Xandromus <xanderrapstine@gmail.com>
2018-05-02 14:46:42 -0500

    set up database for JAWS_DB

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,25 @@
 require("dotenv").config();
 const mysql = require("mysql");
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
+let connection;
 
-    // username
-    user: "root",
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
 
-    // password
-    password: process.env.MYSQLPASSWORD,
-    database: "burgers_db"
-});
+        // username
+        user: "root",
+
+        // password
+        password: process.env.MYSQLPASSWORD,
+        database: "burgers_db"
+    });
+};
 
 // Make connection.
-connection.connect(function (error) {
-    if (error) {
-        console.error("error connecting: " + error.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 
 module.exports = connection;
